fbaa7ecdf4e3530cbb4a8917ef2be9def45d1db1
MaximZherlygin <moxem2710@gmail.com>
2022-02-15 22:01:01 +0300

    chlenix

--- lab1/lab1.cu
+++ lab1/lab1.cu
@@ -3,18 +3,17 @@
 
 using namespace std;
 
-__global__ void kernel(double *res, double *arr1, double *arr2, int n) {
+__global__ void kernel(double *arr1, double *arr2, int size) {
     int idx = blockDim.x * blockIdx.x + threadIdx.x;			// Абсолютный номер потока
     int offset = blockDim.x * gridDim.x;						// Общее кол-во потоков
-    for(int i = idx; i < n; i += offset) {
-        res[i] = arr1[i] * arr2[i];
+    for(int i = idx; i < size; i += offset) {
+        arr1[i] = arr1[i] * arr2[i];
     }
 }
 
 int main() {
     int size;
     scanf("%d", &size);
-    double *res = (double *)malloc(sizeof(double) * size);
     double *vec1 = (double *)malloc(sizeof(double) * size);
     double *vec2 = (double *)malloc(sizeof(double) * size);
     for(int i = 0; i < size; i++) {
@@ -24,9 +23,7 @@ int main() {
         scanf("%lf", &vec2[i]);
     }
 
-    double *devRes, *devVec1, *devVec2;
-    cudaMalloc(&devRes, sizeof(double) * size);
-    cudaMemcpy(devRes, res, sizeof(double) * size, cudaMemcpyHostToDevice);
+    double *devVec1, *devVec2;
 
     cudaMalloc(&devVec1, sizeof(double) * size);
     cudaMemcpy(devVec1, vec1, sizeof(double) * size, cudaMemcpyHostToDevice);
@@ -41,7 +38,7 @@ int main() {
     cudaEventCreate(&stop);
     cudaEventRecord(start, 0);
 
-    kernel<<<256, 256>>>(devRes, devVec1, devVec2, size);
+    kernel<<<256, 256>>>(devVec1, devVec2, size);
 
     cudaEventRecord(stop, 0);
     cudaEventSynchronize(stop);
@@ -51,15 +48,13 @@ int main() {
     cudaEventDestroy(start);
 
 
-    // cudaMemcpy(res, devRes, sizeof(double) * n, cudaMemcpyDeviceToHost);
-    cudaFree(devRes);
+    cudaMemcpy(vec1, devVec1, sizeof(double) * size, cudaMemcpyDeviceToHost);
+    for(int i = 0; i < size; i++) {
+        printf("%f ", vec1[i]);
+    }
+    printf("\n");
     cudaFree(devVec1);
     cudaFree(devVec2);
-//    for(int i = 0; i < n; i++) {
-//        printf("%f ", res[i]);
-//    }
-    printf("\n");
-    free(res);
     free(vec1);
     free(vec2);
     return 0;
