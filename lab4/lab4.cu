e7a66744bf8eedf9ff27a6f2604ba4cb647dfd78
MaximZherlygin <moxem2710@gmail.com>
2022-02-20 17:40:32 +0300

    refactored

--- lab4/lab4.cu
+++ lab4/lab4.cu
@@ -8,6 +8,7 @@
 #include <string.h>
 #include <thrust/extrema.h>
 #include <thrust/device_vector.h>
+
 using namespace std;
 
 #define CSC(call)  													\
@@ -22,157 +23,202 @@ do {																\
 
 class Comparator{
 public:
-    __host__ __device__ bool operator()(const double a, const double b) const{
-        return fabs(a) < fabs(b);
+    __device__ __host__ bool operator() (const double& first, const double& second) const {
+        return fabs(first) < fabs(second);
     }
 };
 
-__global__ void swapLines(double* matrix, double* identity, int n, int i, int j) {
+const Comparator comparator;
 
-    int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    int offset = gridDim.x * blockDim.x;
+//__global__ void swapLines(double* matrix, double* identityMatrix, int i, int j, int size) {
+//    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+//    int offset = gridDim.x * blockDim.x;
+//
+//    for (int k = idx; k < size; k += offset) {
+//        double temp = matrix[k * size + i];
+//        matrix[k * size + i] = matrix[k * size + j];
+//        matrix[k * size + j] = temp;
+//
+//        temp = identityMatrix[k * size + i];
+//        identityMatrix[k * size + i] = identityMatrix[k * size + j];
+//        identityMatrix[k * size + j] = temp;
+//    }
+//}
+//
+//
+//__global__ void nullifyDown(double* matrix, double* identityMatrix, int size, int x) {
+//    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+//    int idy = blockIdx.y * blockDim.y + threadIdx.y;
+//    int offsetx = gridDim.x * blockDim.x;
+//    int offsety = gridDim.y * blockDim.y;
+//
+//    for (int i = x + idx + 1; i < size; i += offsetx) {
+//        for (int j = x + idy + 1; j < size; j += offsety) {
+//            matrix[j * size + i] = - matrix[x * size + i] / matrix[x * size + x]
+//                                   * matrix[j * size + x] + matrix[j * size + i];
+//        }
+//        for (int j = idy; j < size; j += offsety) {
+//            identityMatrix[j * size + i] = - matrix[x * size + i] / matrix[x * size + x]
+//                                           * identityMatrix[j * size + x] + identityMatrix[j * size + i];
+//        }
+//    }
+//
+//}
+//
+//__global__ void nullifyUp(double* matrix, double* identityMatrix, int size, int x) {
+//    int idx = threadIdx.x + blockIdx.x * blockDim.x;
+//    int idy = threadIdx.y + blockIdx.y * blockDim.y;
+//    int offsetx = gridDim.x * blockDim.x;
+//    int offsety = gridDim.y * blockDim.y;
+//
+//    for (int i = x - idx - 1; i >= 0; i -= offsetx) {
+//        for (int j = idy; j < size; j += offsety) {
+//            identityMatrix[j * size + i] = - matrix[x * size + i] / matrix[x * size + x]
+//                                           * identityMatrix[j * size + x] + identityMatrix[j * size + i];
+//        }
+//    }
+//}
+//
+//__global__ void divideIdentityMatrix(double* matrix, double* identityMatrix, int size) {
+//    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+//    int idy = blockIdx.y * blockDim.y + threadIdx.y;
+//    int offsetx = gridDim.x * blockDim.x;
+//    int offsety = gridDim.y * blockDim.y;
+//
+//    for (int i = idx; i < size; i += offsetx) {
+//        for (int j = idy; j < size; j += offsety) {
+//            identityMatrix[j * size + i] /= matrix[i * size + i];
+//        }
+//    }
+//}
 
-    int k;
-    double tmp;
-    for (k = idx; k < n; k += offset) {
-        tmp = matrix[k * n + i];
-        matrix[k * n + i] = matrix[k * n + j];
-        matrix[k * n + j] = tmp;
 
-        tmp = identity[k * n + i];
-        identity[k * n + i] = identity[k * n + j];
-        identity[k * n + j] = tmp;
-    }
-}
-
-__global__ void devideIdentity(double* matrix, double* identity, int n) {
+__global__ void kernel (double* matrix, double* identityMatrix, int size, thrust::device_ptr<double> pointer) {
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
     int idy = blockIdx.y * blockDim.y + threadIdx.y;
     int offsetx = gridDim.x * blockDim.x;
     int offsety = gridDim.y * blockDim.y;
-    int i, j;
-    for (i = idx; i < n; i += offsetx) {
-        for (j = idy; j < n; j += offsety) {
-            identity[j * n + i] /= matrix[i * n + i];
+
+    for (int col = 0; col < size - 1; col++) {
+        const int max_idx = thrust::max_element(pointer + col * size + col, pointer + (col + 1) * size, comparator)
+                            - pointer - col * size;
+        if (max_idx != idx){
+            // swapLines<<<256, 256>>>(dev_matrix, dev_identityMatrix, i, max_idx, size);
+            // swapLines(double* matrix, double* identityMatrix, int i, int j, int size)
+
+            // Свапаем местами строки (если максимальный элемент стоит не на главной диагонали)
+            for (int k = idx; k < size; k += offset) {
+                double tempMatrixValue = matrix[k * size + col];
+                matrix[k * size + col] = matrix[k * size + max_idx];
+                matrix[k * size + max_idx] = tempMatrix;
+
+                double tempIdentityMatrixValue = identityMatrix[k * size + col];
+                identityMatrix[k * size + col] = identityMatrix[k * size + max_idx];
+                identityMatrix[k * size + max_idx] = tempIdentityMatrixValue;
+            }
+        }
+
+        // Зануляем элементы ниже главного
+        // makeDownNull<<<block(32, 16), thread(32, 16)>>>(dev_matrix, dev_identityMatrix, size, i);
+        // nullifyDown(double* matrix, double* identityMatrix, int size, int x)
+        for (int i = col + idx + 1; i < size; i += offsetx) {
+            for (int j = col + idy + 1; j < size; j += offsety) {
+                matrix[j * size + i] = - matrix[col * size + i] / matrix[col * size + col]
+                                       * matrix[j * size + col] + matrix[j * size + i];
+            }
+            for (int j = idy; j < size; j += offsety) {
+                identityMatrix[j * size + i] = - matrix[col * size + i] / matrix[col * size + col]
+                                               * identityMatrix[j * size + col] + identityMatrix[j * size + i];
+            }
         }
     }
-}
 
-__global__ void makeDownNull(double* matrix, double* identity, int n, int x) {
-    int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    int idy = blockIdx.y * blockDim.y + threadIdx.y;
-    int offsetx = gridDim.x * blockDim.x;
-    int offsety = gridDim.y * blockDim.y;
-
-    int i, j;
-    double particion;
-    for (i = x + 1 + idx; i < n; i += offsetx) {
-        particion = - matrix[x * n + i] / matrix[x * n + x];
-        for (j = x + 1 + idy; j < n; j += offsety) {
-            // matrix[j * n + i] = fma(-matrix[x * n + i] / matrix[x * n + x], matrix[j * n + x], matrix[j * n + i]);
-            matrix[j * n + i] = particion * matrix[j * n + x] + matrix[j * n + i];
-        }
-        for (j = idy; j < n; j += offsety) {
-            // identity[j * n + i] = fma(-matrix[x * n + i] / matrix[x * n + x], identity[j * n + x], identity[j * n + i]);
-            identity[j * n + i] = particion * identity[j * n + x] + identity[j * n + i];
+    for (int col = size - 1; col > 0; col--) {
+        // Зануляем всё выше главной диагонали
+        // makeUpNull<<<block(32, 16), thread(32, 16)>>>(dev_matrix, dev_identityMatrix, size, i);
+        // nullifyUp(double* matrix, double* identityMatrix, int size, int x)
+        for (int i = col - idx - 1; col >= 0; col -= offsetx) {
+            for (int j = idy; j < size; j += offsety) {
+                identityMatrix[j * size + i] = - matrix[col * size + i] / matrix[col * size + col]
+                                               * identityMatrix[j * size + col] + identityMatrix[j * size + i];
+            }
         }
     }
 
-}
-
-__global__ void makeUpNull(double* matrix, double* identity, int n, int x) {
-    int idx = threadIdx.x + blockIdx.x * blockDim.x;
-    int idy = threadIdx.y + blockIdx.y * blockDim.y;
-    int offsetx = gridDim.x * blockDim.x;
-    int offsety = gridDim.y * blockDim.y;
-
-    int i, j;
-    double particion;
-    for (i = x - 1 - idx; i >= 0; i -= offsetx) {
-        particion = -matrix[x * n + i] / matrix[x * n + x];
-        for (j = idy; j < n; j += offsety) {
-            // identity[j * n + i] = fma(-matrix[x * n + i] / matrix[x * n + x], identity[j * n + x], identity[j * n + i]);
-            identity[j * n + i] = particion * identity[j * n + x] + identity[j * n + i];
+    // Делим строки присоединённой матрицы на соотв. элемент с главной диагонали главной матрицы
+    // divideIdentityMatrix<<<block(32, 16), thread(32, 16)>>>(dev_matrix, dev_identityMatrix, size);
+    for (int i = idx; i < size; i += offsetx) {
+        for (int j = idy; j < size; j += offsety) {
+            identityMatrix[j * size + i] /= matrix[i * size + i];
         }
     }
 }
 
 
 int main() {
-
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(NULL);
 
-    int n;
-    cin >> n;
-    if( n <= 0 ){
-        return 0;
-    }
+    int size;
+    cin >> size;
 
-    int i, j;
-    double *matrix = (double*)malloc( n * n * sizeof(double));
-    double *identity = (double*)malloc( n * n * sizeof(double));
+    const int array_size = size * size;
 
-    for (i = 0; i < n; ++i) {
-        for (j = 0; j < n; ++j) {
-            cin >> matrix[j * n + i];
+    double *matrix = (double*)malloc( array_size * sizeof(double));
+    double *identityMatrix = (double*)malloc( array_size * sizeof(double));
+
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            cin >> matrix[j * size + i];
         }
     }
 
-    for (i = 0; i < n; ++i) {
-        for (j = 0; j < n; ++j) {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
             i != j
-                ? identity[i * n + j] = 0.0
-                : identity[i * n + j] = 1.0;
+                ? identityMatrix[i * size + j] = 0.0
+                : identityMatrix[i * size + j] = 1.0;
         }
     }
-
-
 
     double* dev_matrix = 0;
-    double* dev_identity = 0;
-    CSC(cudaMalloc(&dev_matrix, sizeof(double) * n * n));
-    CSC(cudaMalloc(&dev_identity, sizeof(double) * n * n));
-    cudaMemcpy(dev_matrix, matrix, sizeof(double) * n * n, cudaMemcpyHostToDevice);
-    cudaMemcpy(dev_identity, identity, sizeof(double) * n * n, cudaMemcpyHostToDevice);
-    const thrust::device_ptr<double> ptr = thrust::device_pointer_cast(dev_matrix);
+    double* dev_identityMatrix = 0;
+    CSC(cudaMalloc(&dev_matrix, sizeof(double) * array_size));
+    CSC(cudaMalloc(&dev_identityMatrix, sizeof(double) * array_size));
+    CSC(cudaMemcpy(dev_matrix, matrix, sizeof(double) * array_size, cudaMemcpyHostToDevice));
+    CSC(cudaMemcpy(dev_identityMatrix, identityMatrix, sizeof(double) * array_size, cudaMemcpyHostToDevice));
+    const thrust::device_ptr<double> pointer = thrust::device_pointer_cast(dev_matrix);
 
-    const Comparator comp;
-    dim3 block(32, 16);
-    dim3 thread(32, 16);
+//    for (int i = 0; i < size - 1; i++) {
+//        const int max_idx = thrust::max_element(pointer + i * size + i, pointer + (i + 1) * size, comparator) - pointer - i * size;
+//        if (max_idx != i) swapLines<<<256, 256>>>(dev_matrix, dev_identityMatrix, i, max_idx, size);
+//        makeDownNull<<<block(32, 16), thread(32, 16)>>>(dev_matrix, dev_identityMatrix, size, i);
+//    }
+//
+//    for (int i = size - 1; i > 0; i--) {
+//        makeUpNull<<<block(32, 16), thread(32, 16)>>>(dev_matrix, dev_identityMatrix, size, i);
+//    }
+//
+//    divideIdentityMatrix<<<block(32, 16), thread(32, 16)>>>(dev_matrix, dev_identityMatrix, size);
 
-    for (i = 0; i < n - 1; ++i) {
-        const int max_idx = thrust::max_element(ptr + i * n + i, ptr + (i + 1) * n, comp) - ptr - i * n;
-        if (max_idx != i){
-            swapLines<<<256, 256>>>(dev_matrix, dev_identity, n, i, max_idx);
-        }
-        makeDownNull<<<block, thread>>>(dev_matrix, dev_identity, n, i);
-    }
+    kernel<<<block(32, 16), thread(32, 16)>>>(dev_matrix, dev_identityMatrix, size, pointer);
 
-
-    for (i = n - 1; i > 0; i--) {
-        makeUpNull<<<block, thread>>>(dev_matrix, dev_identity, n, i);
-    }
-
-    devideIdentity<<<block, thread>>>(dev_matrix, dev_identity, n);
-
-
-    cudaMemcpy(matrix, dev_matrix, sizeof(double) * n * n, cudaMemcpyDeviceToHost);
-    cudaMemcpy(identity, dev_identity, sizeof(double) * n * n, cudaMemcpyDeviceToHost);
-    cudaFree(dev_matrix);
-    cudaFree(dev_identity);
+    CSC(cudaMemcpy(matrix, dev_matrix, sizeof(double) * array_size, cudaMemcpyDeviceToHost));
+    CSC(cudaMemcpy(identityMatrix, dev_identityMatrix, sizeof(double) * array_size, cudaMemcpyDeviceToHost));
 
     cout << scientific;
     cout.precision(10);
-    for (i = 0; i < n; ++i) {
-        for (j = 0; j < n; ++j) {
-            cout << identity[j * n + i] << " ";
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            cout << identityMatrix[i + j * size] << ' ';
         }
-        cout << "\n";
+        cout << '\n';
     }
 
+    cudaFree(dev_matrix);
+    cudaFree(dev_identityMatrix);
     delete[] matrix;
-    delete[] identity;
+    delete[] identityMatrix;
 
     return 0;
 }
